31969991d39acef4651b60b8848b4e35789c0a44
Yaroslav Klimenko <klimyarik13@gmail.com>
2025-01-06 18:34:07 +0200

    auth(front) connected to back

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -2,14 +2,11 @@ import { useSelector } from "react-redux";
 import { selectIsLoggedIn, selectIsRefreshing } from "../redux/auth/selectors";
 import { Navigate } from "react-router-dom";
 
-export default function PrivateRoute({
-  redirectTo = "/landing",
-  component: Component,
-}) {
+export default function PrivateRoute({ redirectTo = "/landing", component }) {
   const isLoggedIn = useSelector(selectIsLoggedIn);
   const isRefreshing = useSelector(selectIsRefreshing);
 
   const requireRedirect = !isLoggedIn && !isRefreshing;
 
-  return requireRedirect ? <Navigate to={redirectTo} /> : <Component />;
+  return requireRedirect ? <Navigate to={redirectTo} /> : component;
 }
